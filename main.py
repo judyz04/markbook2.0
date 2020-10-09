26ba6ced2fced1209577dde989ea88b4e4b8e8f3
Judy Zhou <di.zhou22@ycdsbk12.ca>
2020-10-09 15:05:26 +0000

    make Classes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from typing import List
+
 class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+
+        def __repr__(self):
+            return f"Person({self.name}, {self.age})"
 
 class Student:
     pass
@@ -10,12 +15,20 @@ class Teacher:
     pass
 
 class Classroom:
-    def __init__(self, course_name: str, course_code: str, period: int, teacher: Teacher, student: Student):
+    class_list = []
+
+    def __init__(self, course_name: str, course_code: str, period: int, teacher: Teacher, class_list: List):
         self.course_name = course_name
         self.course_code = course_code
         self.period = period
-        self.teacher = teacher
-        self.student = student
+
+    def add_student(cls, student: 'Student'):
+        cls.class_list.append(student)
+    
+    def __repr__(self):
+            return f"Student(Name: {self.name}, Age:{self.age})"
+    
+
 
 class Assignment:
     pass
