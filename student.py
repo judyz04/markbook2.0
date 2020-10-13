3d96c61a22dc380889d1c5cbb5290b1402cc6de1
Erica Han <erica.han22@ycdsbk12.ca>
2020-10-13 03:53:29 +0000

    class student

--- student.py
+++ student.py
@@ -1,6 +1,19 @@
 from main import *
 from person import *
 
-class Student(Person):
-     def __init__(self, name: str, age: int, student_number: int):
-        super().__init__(name, age)
+class Student:
+    """
+    attributes: name, grade, teacher & assignment.
+    methods: get grade, show student's assignments, show student's teacher.
+    """
+    def __init__(self, first_name: str, last_name: str, grade: int, teacher: str, assignment: List[Assignment]) :
+        """ 
+        args:
+                first_name(str): student's first name
+                last_name(str): student's last name
+                grade(int): student's grade
+                teacher(str): student's teacher
+                assignment(List[Assignment]): list of assignments student has
+        Return -> None
+        """
+        
