b62682b9e67501dc50e3a6466725661348013f4a
ドラゴンが好き <apatosaurus2003@gmail.com>
2020-10-21 09:00:09 -0400

    おはよお

--- assignment.py
+++ assignment.py
@@ -2,5 +2,19 @@ from main import *
 
 
 class Assignment:
-    pass
-
+    
+    def __init__(self, name:str, info:str, classroom:Classroom):
+        self.name = name
+        self.info = info
+        self.classroom = classroom
+               
+    def assign_assignment(self, student:Student):
+        student.assignments.append(self)
+        
+    def get_info(self):
+        print(self.name)
+        print('一一一一一一一一一一一一一')
+        print(self.info)
+        
+    def mark(self, student:Student, mark:float):
+        pass
